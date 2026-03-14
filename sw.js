f9a7f28697105893a7b4d3b34b5e5418a8f58533
saktachan325 <saktachan@gmail.com>
2026-03-14 17:08:57 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = 'jurnal harian uin madura-v1773482737600';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting(); // Force the waiting service worker to become the active service worker
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // Delete old caches
+          }
+        })
+      );
+    }).then(() => self.clients.claim()) // Take control of all clients immediately
+  );
+});
+
+self.addEventListener('fetch', event => {
+  // Network First, falling back to cache strategy
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+
+        // IMPORTANT: Clone the response. A response is a stream
+        // and because we want the browser to consume the response
+        // as well as the cache consuming the response, we need
+        // to clone it so we have two streams.
+        const responseToCache = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+
+        return response;
+      })
+      .catch(() => {
+        // If network fails, try to get it from the cache
+        return caches.match(event.request);
+      })
+  );
+});
